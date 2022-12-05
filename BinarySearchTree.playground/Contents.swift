ce3c46f86f40377d76d15e7fe43915e57107378c
thutasann <thutasann@gmail.com>
2022-12-05 22:01:24 +0630

    Binary search Tree part2

--- BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree.playground/Contents.swift
@@ -1,208 +1,319 @@
-
-// MARK: - BINARY SEARCH TREE
-class BinarySearchTree{
+class BinarySearchTree {
+    
+    // MARK: - Properties
     
     var value: Int
     var left: BinarySearchTree?
     var right: BinarySearchTree?
     var parent: BinarySearchTree?
     
-    // Initialize the value
-    init(value: Int){
-        self.value = value
+    var isLeaf: Bool {
+        return right == nil && left == nil
     }
     
-    // isLeaf
-    var isLeaf: Bool {
-        return left == nil && right == nil
+    var isLeft: Bool {
+        return value < parent!.value
+    }
+    
+    var isRight: Bool {
+        return value > parent!.value
+    }
+    
+    var hasRight: Bool {
+        return right != nil
+    }
+    
+    var hasLeft: Bool {
+        return left != nil
+    }
+    
+    var hasOneChild: Bool {
+        return hasRight || hasLeft
+    }
+    
+    var hasTwoChildren: Bool {
+        return hasRight && hasLeft
+    }
+    
+    // MARK: - Init
+    
+    init(value: Int) {
+        self.value = value
     }
 }
 
-
-// MARK: - INSERT
-extension BinarySearchTree{
+// MARK: - Insert Value
+extension BinarySearchTree {
     
-    // Insert parent
-    func insert(value: Int){
+    func insert(value: Int) {
         insert(value: value, parent: self)
     }
     
-    // Intset Nodes to Our Binary Search Tree
-    private func insert(value: Int, parent: BinarySearchTree){
-    
-        // left Node
-        if (value < self.value){
-            
-            if let left = left{ // check left node alrdy existed
-                left.insert(value: value, parent: self) // recursion if existed
-            } else{
-                left = BinarySearchTree(value: value)
-            }
-            
+    func insert(values: [Int]) {
+        values.forEach { (value) in
+            insert(value: value)
         }
-        // right node
-        else{
-            
-            if let right = right{ // check right node alrdy existed
-                right.insert(value: value, parent: self) // recursion if existed
-            } else{
-                right = BinarySearchTree(value: value)
-            }
-            
-        }
-        
     }
     
+    private func insert(value: Int, parent: BinarySearchTree?) {
+        if value < self.value {
+            if let left = left {
+                left.insert(value: value)
+            } else {
+                left = BinarySearchTree(value: value)
+                left?.parent = self
+            }
+        } else {
+            if let right = right {
+                right.insert(value: value)
+            } else {
+                right = BinarySearchTree(value: value)
+                right?.parent = self
+            }
+        }
+    }
 }
 
-// MARK: - SEARCH
-extension BinarySearchTree{
+// MARK: - Search
+extension BinarySearchTree {
     
-    // Search and return Tree
-    func searchAndReturnTree(forValue value: Int) -> BinarySearchTree?{
-        
-        if value == self.value{
-            print(self)
+    func searchAndReturnTree(forValue value: Int) -> BinarySearchTree? {
+        if value == self.value {
             return self
         }
         
         if value < self.value {
-            if let left = left{
-                return left.searchAndReturnTree(forValue: value)
-            }
+            guard let left = left else { return nil }
+            return left.searchAndReturnTree(forValue: value)
         } else {
-            if let right = right {
-                return right.searchAndReturnTree(forValue: value)
-            }
+            guard let right = right else { return nil }
+            return right.searchAndReturnTree(forValue: value)
         }
-        return nil
     }
     
-    // Search Value
-    func search (forValue value: Int) -> Bool{
-        
-        if value == self.value{
+    func search(forValue value: Int) -> Bool {
+        if value == self.value {
             return true
         }
         
         if value < self.value {
-            
-            if let left = left{
-                return left.search(forValue: value)
-            }
-            
+            guard let left = left else { return false }
+            return left.search(forValue: value)
         } else {
-            
-            if let right = right {
-                return right.search(forValue: value)
-            }
-            
+            guard let right = right else { return false }
+            return right.search(forValue: value)
         }
-        
-        return false
     }
-    
 }
 
-// MARK: - DETERMINE THE HEIGHT OF OUR TREE AND THE DEPTH OF EACH PARTICULAR NODES
-extension BinarySearchTree{
+// MARK: - Min/Max/Height
+extension BinarySearchTree {
     
-    // minimum -> left most node
-    func minimum() -> Int{
-        
+    func minimum() -> BinarySearchTree {
         var node = self
         
-        while let left = node.left {
-            node = left
+        while node.left != nil {
+            node = node.left!
         }
-        return node.value
-        
+        return node
     }
     
-    // maximum -> right most node
-    func maximum() -> Int {
-        
+    func maximum() -> BinarySearchTree {
         var node = self
         
-        while let right = node.right{
-            node = right
+        while node.right != nil {
+            node = node.right!
         }
-        return node.value
-        
+        return node
     }
     
-    // height of our particular node
     func height() -> Int {
-        if tree.isLeaf{
+        if isLeaf {
             return 0
-        } else{
+        } else {
             return 1 + max(left?.height() ?? 0, right?.height() ?? 0)
         }
     }
     
-    // depth of our particular node
-    func depth(ofValue value : Int) -> Int {
+    func depth(ofValue value: Int) -> Int {
         var depth = 0
         
-        guard var tree = searchAndReturnTree(forValue: value) else { return 0}
+        guard var tree = searchAndReturnTree(forValue: value) else { return depth }
         
-        while let parent = tree.parent{
+        while let parent = tree.parent {
             tree = parent
             depth += 1
         }
+        
         return depth
     }
-    
 }
 
-
-// MARK: - STRING CONVERTIBLE PROTOCOL (when converting instance to String)
-extension BinarySearchTree: CustomStringConvertible{
+// MARK: - Delete
+extension BinarySearchTree {
     
-    var description: String{
+    func remove(value: Int) {
+        guard let tree = searchAndReturnTree(forValue: value) else { return }
         
-        var result = ""
-        
-        if let left = left{
-            result += "\(left.description) <- "
+        if tree.hasTwoChildren {
+            tree.removeNodeWithTwoChildren(tree: tree)
+            return
         }
         
-        result += "\(value)"
-        
-        if let right = right{
-            result += " -> \(right.description)"
+        if tree.hasOneChild {
+            tree.removeNodeWithOneChild(tree: tree)
+            return
         }
         
-        return result
+        if tree.isLeaf {
+            tree.removeLeaf(tree: tree)
+            return
+        }
+    }
+    
+    private func removeLeaf(tree: BinarySearchTree) {
+        if tree.isLeft {
+            tree.parent?.left = nil
+        } else {
+            tree.parent?.right = nil
+        }
+    }
+    
+    private func removeNodeWithOneChild(tree: BinarySearchTree) {
+        if tree.isLeft {
+            if tree.hasLeft {
+                tree.parent?.left = tree.left
+            } else {
+                tree.parent?.left = tree.right
+            }
+        }
+        if tree.isRight {
+            if tree.hasLeft {
+                tree.parent?.right = tree.left
+            } else {
+                tree.parent?.right = tree.right
+            }
+        }
+    }
+    
+    private func removeNodeWithTwoChildren(tree: BinarySearchTree) {
+        guard let replacementTree = searchAndReturnTree(forValue: (tree.right?.minimum().value)!) else { return }
+        replaceNode(withTree: replacementTree, parentTree: tree)
+    }
+    
+    private func replaceNode(withTree tree: BinarySearchTree, parentTree: BinarySearchTree) {
+        if parentTree.isLeft {
+            if tree.parent?.value == parentTree.value {
+                parentTree.parent?.left = tree
+                tree.left = parentTree.left
+            } else {
+                tree.parent?.left = nil
+                tree.right = parentTree.right
+                tree.left = parentTree.left
+                parentTree.parent?.left = tree
+            }
+        } else {
+            if tree.parent?.value == parentTree.value {
+                parentTree.parent?.right = tree
+                tree.left = parentTree.left
+            } else {
+                tree.parent?.left = nil
+                tree.right = parentTree.right
+                tree.left = parentTree.left
+                parentTree.parent?.right = tree
+            }
+        }
+    }
+}
+
+// MARK: - Traversal
+extension BinarySearchTree {
+    
+    // left -> root -> right
+    func inOrderTraversal() -> [Int] {
+        var resultArray = [Int]()
         
+        if let left = left {
+            resultArray += left.inOrderTraversal()
+        }
+        
+        resultArray.append(self.value)
+        
+        if let right = right {
+            resultArray += right.inOrderTraversal()
+        }
+        return resultArray
+    }
+    
+    // root -> left -> right
+    func preOrderTraversal() -> [Int] {
+        var resultArray = [self.value]
+        
+        if let left = left {
+            resultArray += left.preOrderTraversal()
+        }
+        
+        if let right = right {
+            resultArray += right.preOrderTraversal()
+        }
+        
+        return resultArray
+    }
+    
+    // left -> right -> root
+    func postOrderTraversal() -> [Int] {
+        var resultArray = [Int]()
+        
+        if let left = left {
+            resultArray += left.postOrderTraversal()
+        }
+        
+        if let right = right {
+            resultArray += right.postOrderTraversal()
+        }
+        
+        resultArray.append(value)
+        
+        return resultArray
     }
 }
 
 
-// MARK: - RUN THE PLAYGROUND
+// MARK: - Description
+extension BinarySearchTree: CustomStringConvertible {
+    var description: String {
+        var text = ""
+        
+        if let left = left {
+            text += "("
+            text += left.description
+            text += ") <- "
+        }
+        
+        text += "\(value)"
+        
+        if let right = right {
+            text += " -> ("
+            text += right.description
+            text += ")"
+        }
+        return text
+    }
+}
 
-let tree = BinarySearchTree(value: 8)
+let values = [2,1,3,12,9,21,19,25]
 
-tree.insert(value: 6)
-tree.insert(value: 10)
-
-tree.insert(value: 4)
-tree.insert(value: 12)
-
+let tree = BinarySearchTree(value: 7)
 tree.insert(value: 2)
-tree.insert(value: 14)
+tree.insert(value: 1)
+tree.insert(value: 4)
+tree.insert(value: 3)
+tree.insert(value: 5)
+tree.insert(value: 10)
+tree.insert(value: 9)
+tree.insert(value: 12)
+tree.insert(value: 11)
+tree.insert(value: 13)
 
-print("tree", tree)
-
-//tree.search(forValue: 14)
-
-//tree.searchAndReturnTree(forValue: 6)
-tree.depth(ofValue: 4)
-
-//tree.minimum()
-//tree.maximum()
-
-//print("tree height", tree.height())
-
-
-// Algorithms Explained at - https://www.geeksforgeeks.org/binary-search-tree-data-structure/
+tree.remove(value: 4)
+print(tree)
